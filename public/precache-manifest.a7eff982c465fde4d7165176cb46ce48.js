a47ffbaa9e5dfdea9965038a8a459aa8a907dd61
JustinWeicht <justinweicht11@gmail.com>
2021-09-16 11:58:26 -0400

    cloned old commit

---
+++ public/precache-manifest.a7eff982c465fde4d7165176cb46ce48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765bfe3f4c9501a6203dff02af9ece80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "221bd30cff38df1da69e",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "d3d2f1dea2c02801de44",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "221bd30cff38df1da69e",
+    "url": "/static/js/2.a69ecbcc.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.a69ecbcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d2f1dea2c02801de44",
+    "url": "/static/js/main.c02dccd3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
