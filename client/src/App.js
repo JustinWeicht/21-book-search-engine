81a4bdacb7df68d312ada598f518d8d0809c8b02
JustinWeicht <justinweicht11@gmail.com>
2021-09-16 10:20:42 -0400

    working locally, minus login/signup

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,25 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+import { ApolloProvider } from '@apollo/client';
+import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
 });
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+<ApolloProvider client={client}>
       <Router>
         <>
           <Navbar />
@@ -44,7 +33,7 @@ function App() {
           </Switch>
         </>
       </Router>
-    </ApolloProvider>
+</ApolloProvider>
   );
 }
 
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,20 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
   const { loading, data } = useQuery(GET_ME);
-
   const userData = data?.me || [];
 
   const { removeBook } = useMutation(REMOVE_BOOK);
-
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
 
   // Removed useEffect() Hook
 
@@ -39,7 +37,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
